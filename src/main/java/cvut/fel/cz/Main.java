e48c5678352beab228ff05e6de4c5e54fd3073cc
Alisa Skulakova <skulaali@fel.cvut.cz>
2024-04-10 23:38:52 +0200

    First commit

---
+++ src/main/java/cvut/fel/cz/Main.java
@@ -0,0 +1,14 @@
+package cvut.fel.cz;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * пишем начальные три символа и появляется комментарий
+         * здесь короче должно открытваться окно и запускаться симуляция
+         * set initial parametrs
+         *
+         */
+    }
+}
